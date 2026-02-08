5e09dd387d7ffbdd87f03e187f41fe3f3cf8bb3e
Muhammad <mohamedkam000@gmail.com>
2026-02-08 06:07:53 +0000

    sync

--- app/src/main/java/com/app/lock/AppLockApplication.kt
+++ app/src/main/java/com/app/lock/AppLockApplication.kt
@@ -4,22 +4,16 @@ import android.app.Application
 import android.content.Context
 import android.os.Build
 import com.app.lock.data.repository.AppLockRepository
-import org.lsposed.hiddenapibypass.HiddenApiBypass
-import rikka.sui.Sui
 
 class AppLockApplication : Application() {
     lateinit var appLockRepository: AppLockRepository
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            HiddenApiBypass.addHiddenApiExemptions("L")
-        }
     }
 
     override fun onCreate() {
         super.onCreate()
         appLockRepository = AppLockRepository(this)
-        Sui.init(packageName)
-    }
+   }
 }
