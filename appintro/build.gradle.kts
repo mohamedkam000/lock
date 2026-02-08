660f1f7866d10f535a10961b12af2ff2b0910e09
Muhammad <mohamedkam000@gmail.com>
2026-02-08 12:35:09 +0000

    sync

--- appintro/build.gradle.kts
+++ appintro/build.gradle.kts
@@ -1,9 +1,6 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -35,12 +32,17 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+    implementation("androidx.activity:activity-compose:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.12.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
+    implementation(platform("androidx.compose:compose-bom:2025.11.01"))
+    implementation("androidx.compose.ui:ui:1.10.0-rc01")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.10.0-rc01")
+    implementation("androidx.compose.material3:material3:1.5.0-alpha09")
+    implementation("androidx.biometric:biometric:1.4.0-alpha04")
     implementation("androidx.compose.material:material-icons-core:1.7.8")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation("androidx.navigation:navigation-compose:2.9.6")
 }
